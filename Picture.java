d9df6176d99a600278313aa0b6d6ef4359385250
Tyrannii <caguilar1243@hotmail.com>
2026-09-14 21:03:42 -0400

    added sun 2
    had to kill Verity to do so, sad times

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        sun2 = new Circle();
         drawn = false;
     }
 
@@ -56,6 +58,12 @@ public class Picture
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            sun2.changeColor("magenta");
+            sun2.moveHorizontal(-100);
+            sun2.changeSize(80);
+            sun2.makeVisible();
+            
             drawn = true;
         }
     }
